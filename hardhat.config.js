b23bcf1a8b6ea5b9fb96ac19228c899e44686fb0
zhijian <gzhijian@outlook.com>
2023-01-13 20:54:16 +0800

    add etherscan verify config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,20 @@
 require("hardhat-deploy");
 require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 module.exports = {
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
   solidity: "0.8.17",
   namedAccounts: {
     deployer: {
@@ -11,5 +23,8 @@ module.exports = {
     buyer: {
       default: 1,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
   },
 };
